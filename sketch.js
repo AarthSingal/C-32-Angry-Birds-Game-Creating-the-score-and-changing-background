822ed995bb5391442d053a2157bb776e09d350df
AarthSingal <aarththestar@gmail.com>
2020-09-29 15:00:09 +0530

    creating the changing BackGroundAndScore

--- sketch.js
+++ sketch.js
@@ -7,11 +7,13 @@ var engine, world;
 var box1, pig1,pig3;
 var backgroundImg,platform;
 var bird, slingshot;
+var bg;
+var score = 0;
 
 var gameState = "onSling";
 
 function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
+    getBackGroundImage();
 }
 
 function setup(){
@@ -45,7 +47,9 @@ function setup(){
 }
 
 function draw(){
-    background(backgroundImg);
+    if(backgroundImg){
+        background(backgroundImg);
+    }
     Engine.update(engine);
     //strokeWeight(4);
     box1.display();
@@ -66,7 +70,17 @@ function draw(){
     bird.display();
     platform.display();
     //log6.display();
-    slingshot.display();    
+    slingshot.display();   
+    
+    pig1.score();
+    pig3.score();
+
+    fill("white");
+    textSize(35);
+    noStroke();
+    text("Score : "+score,width-300,50);
+
+    
 }
 
 function mouseDragged(){
@@ -85,4 +99,17 @@ function keyPressed(){
     if(keyCode === 32){
        // slingshot.attach(bird.body);
     }
+}
+
+async function getBackGroundImage(){
+    var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+    var responseJson = await response.json();
+    var time = responseJson.datetime.slice(11,13);
+    console.log(time);
+    if(time > 6 && time < 18 ){
+        bg = "sprites/bg.png"
+    }else{
+        bg = "sprites/bg2.jpg"
+    }
+    backgroundImg = loadImage(bg);
 }
